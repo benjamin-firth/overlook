8e13b6b2f6819dc153630e9db0e25e43dab188a7
Ben Firth <bfirth93@gmail.com>
2019-11-03 17:49:13 -0700

    Add manager class

--- src/Manager.js
+++ src/Manager.js
@@ -1,4 +1,6 @@
-class Manager extends Hotel {
+import Hotel from '../src/Hotel';
+
+export default class Manager extends Hotel {
   constructor(user, users, bookings, rooms) {
     super(users, bookings, rooms);
     this.id = user.id;
@@ -8,8 +10,29 @@ class Manager extends Hotel {
 
   findTotalRoomsForDate(date) {
     let bookedRooms = this.bookings.filter(booking => booking.date === date);
-    return this.rooms.length - bookedRooms;
+    return this.rooms.length - bookedRooms.length;
   }
 
-  
+  findTotalRevenueForDate(date) {
+    let bookings = this.bookings.filter(booking => booking.date === date);
+    let bookedRooms = bookings.map(booking => {
+      return this.rooms.find(room => {
+        return room.number === booking.roomNumber;
+      })
+    })
+    return bookedRooms.reduce((acc, bookedRoom) => {
+      acc = acc + bookedRoom.costPerNight
+      return acc;
+    }, 0)
+  }
+
+  findPercentageOfRoomsOccupied(date) {
+    let bookings = this.bookings.filter(booking => booking.date === date);
+    let bookedRooms = bookings.map(booking => {
+      return this.rooms.find(room => {
+        return room.number === booking.roomNumber;
+      })
+    })
+    return bookedRooms.length / this.rooms.length * 100;
+  }
 }
