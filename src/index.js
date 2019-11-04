8a3e484512991baab6e86648c2d1741c6ee938be
Ben Firth <bfirth93@gmail.com>
2019-11-03 20:28:08 -0700

    Modify styling for customer page

--- src/index.js
+++ src/index.js
@@ -121,11 +121,11 @@ function renderCustomerPage() {
   let mySpending = customer.findTotalSpent();
   showBookings();
   $('main').html(`
-    <section class="manager-widget booking-list" style="overflow: scroll">
-      <h2>Bookings I've Made</h2>
+    <section class="customer-widget booking-list" style="overflow: scroll">
+      <h2 class="widget-title">Bookings I've Made</h2>
     </section>
-    <section class="manager-widget">
-      <h2>Amount I've Spent</h2>
+    <section class="customer-widget">
+      <h2 class="widget-title">Amount I've Spent</h2>
       <h1>${mySpending}<h1>
     </section>
     <button class="login-return">Return to Main Page</button> 
